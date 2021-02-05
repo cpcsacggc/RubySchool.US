bb74268a2c9c743c97d1b7325dc6c3b2639c4b3b
Sergey Zaikin <seriy1@gmail.com>
2021-02-05 03:34:09 -0800

    chapter 18

---
+++ arc/18/net_demo7.rb
@@ -0,0 +1,24 @@
+﻿require 'net/http'
+require 'uri'
+
+def is_wrong_password? password
+	uri = URI.parse 'http://mynetwd/'
+
+	response = Net::HTTP.post_form(uri, :login => "admin", :password => password).body
+	response.include? "denied"
+
+end
+
+input = File.open "password_2.txt", "r"
+
+while (line = input.gets)
+	line.strip!
+	print "Trying password #{line}... "
+	if is_wrong_password? line
+		puts "Error"
+	else
+		puts "Found"
+		input.close
+		exit
+	end
+end	
---
+++ arc/18/net_demo8.rb
@@ -0,0 +1,12 @@
+﻿require 'net/http'
+require 'uri'
+
+def is_wrong_password password
+	uri = URI.parse 'http://mynetwd/'
+
+	response = Net::HTTP.post_form(uri, :login => "admin", :password => password).body
+	response.include? "denied"
+
+end
+puts is_wrong_password "Number10"
+
