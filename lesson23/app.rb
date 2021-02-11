4b45df96ecbf03d94833cc7e9a07c3b389c47cfa
Sergey Zaikin <seriy1@gmail.com>
2021-02-11 15:11:11 -0800

    lesson 23

--- lesson23/app.rb
+++ lesson23/app.rb
@@ -14,6 +14,15 @@ get '/visit' do
 	erb :visit
 end
 
+get '/contacts' do
+	erb :contacts
+end
+
+get '/result' do
+	@file = File.open("./public/barbershop.txt", "r")
+	erb :result
+end
+
 post '/visit' do
 
 	@username = params[:username]
@@ -22,6 +31,14 @@ post '/visit' do
 	@barber = params[:barber]
 	@color = params[:color]
 
-	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+	#save data to file name
+	f = File.open './public/barbershop.txt', 'a'
+	f.write "User: #{@username}, phone: #{@phone}, date and time: #{@date_time}, Barber: #{@barber}, Color: #{@color}\n"
+	f.close
+
+
+	@title = "Ok"
+	@message = "Username: #{@username}, Phone: #{@phone}, Date and Time: #{@datetime}, Barber: #{@barber}, Color: #{rgb:@color}"
+	erb :message
 
 end
