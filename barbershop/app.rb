fb3470626aa64e0d85db1f1df34da42fd73396a2
Sergey Zaikin <seriy1@gmail.com>
2021-02-12 01:07:22 -0800

    update

--- barbershop/app.rb
+++ barbershop/app.rb
@@ -19,7 +19,16 @@ get '/contacts' do
 end
 
 get '/result' do
+	@arr1 = []
+	@hh = {}
 	@file = File.open("./public/barbershop.txt", "r")
+	@file.each_line do |line|
+		@ar2=[]
+		@arr2 = line.strip
+		@arr1 = @arr2.split(', ')
+		@hh[@file.lineno] = @arr1
+		#@hh.delete_if{|key, value| key == 1}
+	end
 	erb :result
 end
 
@@ -35,6 +44,7 @@ post '/visit' do
 	hh = { 	:username => 'Введите имя',
 			:phone => 'Введите телефон',
 			:datetime => 'Введите дату и время',
+		}
 
 	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
 
@@ -44,8 +54,9 @@ post '/visit' do
 
 	#erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
 	#save data to file name
-	f = File.open './public/barbershop.txt', 'a'
-	f.write "User: #{@username}, phone: #{@phone}, date and time: #{@date_time}, Barber: #{@barber}, Color: #{@color}\n"
+		f = File.open './public/barbershop.txt', 'a'
+
+	f.write "#{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}\n"
 	f.close
 
 
